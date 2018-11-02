dfa17469fcaffe7ee219fa48ce4f917ca3a75129
rias-z <chestermx88@gmail.com>
2018-11-02 17:06:06 +0900

    feat: setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>App</h2>
+      </div>
+    )
+  }
+}
+
+export default App
